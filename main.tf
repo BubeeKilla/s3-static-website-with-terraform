6458b74026765ef085761868420f27088632e177
BubeeKilla <eugengaibu@gmail.com>
2025-08-28 13:12:53 +0200

    Initial static site with Terraform

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "website" {
+  bucket = "eugen-static-site-demo"
+}
+
+# Disable Block Public Access (so we can attach a public policy)
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket                  = aws_s3_bucket.website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Enable static website hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# Allow public GET on objects
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.website.id}/*"
+    }
+  ]
+}
+POLICY
+
+  depends_on = [aws_s3_bucket_public_access_block.website]
+}
+
+# Upload index.html
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+# Output the website URL
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
+}
